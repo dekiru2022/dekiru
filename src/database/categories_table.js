62b1d98d74a51633a2261dd73c5fb071f254081a
GakiyaYukimitsu <yukimitsu1994@gmail.com>
2022-01-12 10:05:25 +0900

    テストデータ：カテゴリの文言修正

--- src/database/categories_table.js
+++ src/database/categories_table.js
@@ -1,19 +1,25 @@
 export const categories = [
   {
     id: 1,
-    category: 'プログラミング',
+    category: '高齢者',
     created_at: '2022-01-01 00:00:01',
     updated_at: '2022-01-01 00:00:01',
   },
   {
     id: 2,
-    category: '介護',
+    category: 'ひとり親',
     created_at: '2022-01-01 00:00:01',
     updated_at: '2022-01-01 00:00:01',
   },
   {
     id: 3,
-    category: '育児',
+    category: '児童',
+    created_at: '2022-01-01 00:00:01',
+    updated_at: '2022-01-01 00:00:01',
+  },
+  {
+    id: 4,
+    category: '高齢者',
     created_at: '2022-01-01 00:00:01',
     updated_at: '2022-01-01 00:00:01',
   }
