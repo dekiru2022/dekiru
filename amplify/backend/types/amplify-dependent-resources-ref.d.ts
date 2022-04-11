348fdeb0bf97854db4e6f663286aa8654a95f24e
inori0927 <>
2022-04-11 21:02:37 +0900

    storage追加

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,5 +16,11 @@ export type AmplifyDependentResourcesAttributes = {
             "AppClientIDWeb": "string",
             "AppClientID": "string"
         }
+    },
+    "storage": {
+        "mydreams": {
+            "BucketName": "string",
+            "Region": "string"
+        }
     }
 }
