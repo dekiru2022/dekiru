0560feb324b33a727307d6f2c15057268b4e929c
inori0927 <>
2022-04-15 00:24:46 +0900

    アップロード機能実装中

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,11 +16,5 @@ export type AmplifyDependentResourcesAttributes = {
             "AppClientIDWeb": "string",
             "AppClientID": "string"
         }
-    },
-    "storage": {
-        "mydreams": {
-            "BucketName": "string",
-            "Region": "string"
-        }
     }
 }
