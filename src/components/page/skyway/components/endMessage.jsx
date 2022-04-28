e7d8c8c5f6a95c8b1005ff86bd4d9a7a019b5e19
GakiyaYukimitsu <yukimitsu1994@gmail.com>
2022-04-29 02:47:30 +0900

    修正

--- src/components/page/skyway/components/endMessage.jsx
+++ src/components/page/skyway/components/endMessage.jsx
@@ -14,9 +14,9 @@ export default function EndMessage(){
     <Box sx={{position: "fixed", top: '50%', left: '50%', transform: 'translate(-50%, -50%)', width: '70vw', height: '70vh', backgroundColor: '#FFF'}} >
       <Stack justifyContent="space-evenly" alignItems="center" direction="column" sx={{ height: "100%"}}>
       制限時間が終了しました。評価アンケートにご協力ください。
-      {isHost == 1
-      ?<Button size='large' variant='contained' color="success" component={LinkRouter} to={`/questionnaire/${questionId}/questioner`} >回答する</Button>
-      :<Button size='large' variant='contained' color="success" component={LinkRouter} to={`/questionnaire/${questionId}/respondent`} >回答する</Button>
+      {//isHost == 1
+      <Button size='large' variant='contained' color="success" component={LinkRouter} to={`/questionnaire/${questionId}/questioner`} >回答する</Button>
+      // :<Button size='large' variant='contained' color="success" component={LinkRouter} to={`/questionnaire/${questionId}/respondent`} >回答する</Button>
       }
       </Stack>
     </Box>
